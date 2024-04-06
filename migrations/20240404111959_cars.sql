88fa9781aa1e901470f9e41dd45f64ed1c860992
Hulla-HooP <shomaaaaaa76@gmail.com>
2024-04-06 18:37:50 +0300

    v1

--- migrations/20240404111959_cars.sql
+++ migrations/20240404111959_cars.sql
@@ -20,6 +20,15 @@ CREATE TABLE cars (
 );
 
 CREATE INDEX car_id_idx ON cars(id);
+CREATE INDEX car_regNum_idx ON cars(regNum);
+CREATE INDEX car_mark_idx ON cars(mark);
+CREATE INDEX car_model_idx ON cars(model);
+CREATE INDEX car_year_idx ON cars(year);
+CREATE INDEX car_name_idx ON cars(name);
+CREATE INDEX car_surname_idx ON cars(surname);
+CREATE INDEX car_patronymic_idx ON cars(patronymic);
+CREATE INDEX car_removed_idx ON cars(removed);
+
 
 -- +goose StatementEnd
 
@@ -28,6 +37,14 @@ CREATE INDEX car_id_idx ON cars(id);
 DROP TABLE cars;
 
 DROP INDEX IF EXISTS car_id_idx;
+DROP INDEX IF EXISTS car_regNum_idx;
+DROP INDEX IF EXISTS car_mark_idx;
+DROP INDEX IF EXISTS car_model_idx;
+DROP INDEX IF EXISTS car_year_idx;
+DROP INDEX IF EXISTS car_name_idx;
+DROP INDEX IF EXISTS car_surname_idx;
+DROP INDEX IF EXISTS car_patronymic_idx;
+DROP INDEX IF EXISTS car_removed_idx;
 
 DROP SEQUENCE car_seq;
 -- +goose StatementEnd
